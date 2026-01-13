6f78c9485b5e14b319b3f1f11d342aa4226579b6
Kuldeep <hi@kuldeep.tech>
2026-01-13 18:27:16 +0530

    feat: update project configuration and asset management
    - Added .assetsignore to exclude _worker.js and _routes.json from asset processing.
    - Removed CNAME file as it is no longer needed.
    - Updated admin config to change repository from coldter/portfolio-site to coldter/kuldeep.tech and set branch to master.
    - Introduced wrangler.jsonc for Cloudflare Workers configuration, specifying main entry point, compatibility date, and asset management settings.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,11 @@
 import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 
+import cloudflare from "@astrojs/cloudflare";
+
 // https://astro.build/config
 export default defineConfig({
-  site: "https://www.aaabbb.com",
+  site: "https://kuldeep.tech",
   integrations: [sitemap()],
+  adapter: cloudflare(),
 });
